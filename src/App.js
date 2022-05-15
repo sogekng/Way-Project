e5fe0352fb7d4cbcfde356858df88d3d33e656ea
sogekng <gutsantos3@gmail.com>
2022-05-15 00:29:53 -0300

    Update build

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import './App.css';
 import React from 'react';
 import Content from './components/Content'
 import Navigator from './components/Navigator'
-import DarkTheme from './components/DarkTheme';
+import Header from './components/Header';
 
 function App() {
   
   return (
     <>
-    <DarkTheme />
+    <Header />
       <Content />
     <Navigator />
     </>
