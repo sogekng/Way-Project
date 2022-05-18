71a4fe675f384940542370119aaf8742ed8bcf9d
sogekng <gutsantos3@gmail.com>
2022-05-17 21:06:20 -0300

    Update

--- src/globals.js
+++ src/globals.js
@@ -8,10 +8,6 @@ box-sizing: border-box;
 body {
   background: ${({ theme }) => theme.body};
   color: ${({ theme }) => theme.text};
-  margin: 0;    
-  padding: 0;    
-  font-family: sans-serif;    
-  transition: all 0.25s linear;
 }
 
 .header{
